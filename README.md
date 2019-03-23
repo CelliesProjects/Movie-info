a3ce12b6195c92991db87e8f13d0dcc3f9531734
Cellie <CelliesProjects@users.noreply.github.com>
2019-03-23 16:35:19 +0100

    Added a searchfield and its handler.
    Added a search icon from material.io.
    
    Changed the font to Google Roboto font.
    
    Updated README.md to reflect these changes.
    
    Removed a lot of whitespace.

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ A single file PHP/jQuery webpage to see some info about your locally stored movi
 <br>See it in action [here](https://films.wasietsmet.nl).
 ### How to install?
 1. Copy `index.htm` to your LAMP webhost.
-2. Change the `$api_key` variable (in `index.htm`) to your themoviedb.org API key or (better yet) store it outside your webroot and include it. (For example with `trim(file_get_contents( ... ))`).
+2. Replace the `$api_key` variable (in `index.htm`) with your own themoviedb.org API key or (better yet) store it outside your webroot and include it. (For example with `trim(file_get_contents( ... ))`).
 3. Change the `$watchedFolder` variable (in `index.htm`) to the folder where your movies are stored.
+
 ### Things to note:
 - Use separate movie folders.
 <br>Movie-info assumes your movies are stored in seperate subfolders and uses the foldernames as the argument for the search at `themoviedb.org`.
@@ -16,8 +17,13 @@ A single file PHP/jQuery webpage to see some info about your locally stored movi
 <br>You will have to provide your own.
 <br>Getting a key is free (as in beer) and easy, just sign up at [themoviedb.org.](https://www.themoviedb.org/account/signup)
 
-Movie-info is developed on/for Linux. It probably needs some work to run on Windows.
-#### Screenshots:
+### Used open source libraries:
+- The used icons are from [material.io](https://material.io/tools/icons/?style=baseline) and are [available under Apache2.0 license](https://www.apache.org/licenses/LICENSE-2.0.html).
+- Uses [Google Roboto font](https://fonts.google.com/specimen/Roboto) which is [available under Apache2.0 license](https://www.apache.org/licenses/LICENSE-2.0.html).
+- Uses [jQuery 3.3.1](https://code.jquery.com/jquery-3.3.1.js) which is [available under MIT license](https://jquery.org/license/).
+
+Movie-info is developed on/for Linux.
+### Screenshots:
 
 Windowed:
 <img src="https://cloud.githubusercontent.com/assets/24290108/24064644/f92ba2d4-0b65-11e7-949c-70aaccfd2aaf.png" />
--- index.php
+++ index.php
@@ -12,8 +12,18 @@ if (isset($_GET['cast']))
   echo file_get_contents("https://api.themoviedb.org/3/movie/".urlencode( $_GET['cast'] )."/casts?api_key=".$api_key);
   die();
 }
+if (isset($_GET['icon']))
+{
+  if ($_GET['icon']=='search')
+ {
+    header("Content-Type: image/svg+xml");
+    echo '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
+    //https://material.io/tools/icons/?icon=search&style=baseline
+    die();
+  }
+  die('invalid icon');
+}
 if ( count($_GET) ) die('ERROR unknown request.');
-
 $config = json_decode(file_get_contents('https://api.themoviedb.org/3/configuration?&api_key='.$api_key),true);
 $info = $config['images'];
 $secure_base_url = $info['secure_base_url'].end($info['backdrop_sizes']);
@@ -22,105 +32,135 @@ $secure_base_url = $info['secure_base_url'].end($info['backdrop_sizes']);
 <html lang="en">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=.7, maximum-scale=.7, user-scalable=no">
 <title>Movies <?php echo $programversion; ?></title>
 <meta name="description" content="Movies">
 <meta name="author" content="Cellie">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">  <!-- https://fonts.google.com/specimen/Roboto?selection.family=Roboto -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <style>
-html, body {
+html, body{
   min-height:100%;
   height:100%;
   margin:0;
   color:yellow;
-  font-variant: small-caps;
   background-color:black;
   font-size:20px;
+  font-family:'Roboto', sans-serif;
 }
-a {
+a{
   color:yellow;
   text-decoration:none;
 }
 #viewport{
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background: no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  position:fixed;
+  top:0;
+  left:0;
+  bottom:0;
+  right:0;
+  background:no-repeat center center fixed;
+  -webkit-background-size:cover;
+  -moz-background-size:cover;
+  -o-background-size:cover;
+  background-size:cover;
 }
-#movieList{
+#movietitle{
+  position:absolute;
+  top:0;
+  left:0;
+  width:70%;
+  background-color:black;
+  text-align:center;
+  color:yellow;
+  font-size:2em;
+  opacity:0.7;
+  height:60px;
+  line-height:60px;
+}
+#searchBox{
+  width:30%;
+  height:60px;
   position:absolute;
   top:0;
   right:0;
+  display:flex;
+}
+#searchText{
+  width:100%;
+  text-align:center;
+  background:gray;
+  font-size:xx-large;
+  color:yellow;
+  opacity:0.7;
+  border:none;
+  }
+#searchButton{
+  cursor:pointer;
+  width:70px;
+  min-width:70px;
+  min-width:70px;
+  background-color:yellow;
+  opacity:0.5;
+}
+#movieList{
+  position:absolute;
+  top:60px;
+  right:0;
   bottom:0;
-  width:30%;
+  width:20%;
   overflow-Y:auto;
   text-align:center;
   background-color:black;
   opacity:0.7;
 }
-#movieList p.item{
+.item{
   padding:15px;
-  font-size:1.3em;
-  color: yellow;
-}
-#movietitle {
-  position: absolute;
-  top:0;
-  left:0;
-  width: 70%;
-  background-color: black;
-  padding: 10px 0;
-  text-align:center;
   color:yellow;
-  font-size:2em;
-  opacity:0.7;
 }
-#movieinfo {
-  position: absolute;
-  bottom: 0;
-  max-height: 50%;
-  background-color: black;
-  left: 0;
-  right: 0;
-  width:70%;
+.item:hover{
+  cursor:pointer;
+  background-color:green;
+}
+#movieinfo{
+  position:absolute;
+  bottom:0;
+  max-height:50%;
+  background-color:black;
+  left:0;
+  right:20%;
   opacity:0.7;
   overflow-y:auto;
 }
 #movieinfo p{
-  margin: 10px;
-  font-size:20px;
+  margin:0 5px;
 }
-#movieList p.item:hover{
-  cursor:pointer;
-}
-#attribution, #moviedata {
+#attribution, #moviedata{
   text-align:right;
+  font-size:smaller;
 }
-#noimage {
-  font-size: 2em;
-  position: absolute;
-  top: 30%;
-  transform: translateY(-50%);
-  left: 50%;
-  transform: translateX(-50%);
+#noimage{
+  font-size:2em;
+  position:absolute;
+  top:30%;
+  transform:translateY(-50%);
+  left:50%;
+  transform:translateX(-50%);
 }
-#castLink {
+#castLink{
   cursor:pointer;
 }
 #castBox{
-  text-align: center;
+  text-align:center;
 }
 </style>
 </head>
 <body>
 <div id="viewport">
+<div>
 <div id="movietitle"></div>
-<div id="movieList"><?php
+<div id="searchBox"><input id="searchText" type="text"><img src="?icon=search" id="searchButton"></div>
+</div>
+<div id="movieList"><p style="color:white;">Local movies:</p><?php
 exec('ls '.$watchedFolder,$movie_array);
 $badchars = array(".","_");                                              #filenamen opschonen, alle '_' en '.' vervangen door spaties
 foreach($movie_array as $film_name)
@@ -131,13 +171,14 @@ foreach($movie_array as $film_name)
 </div>
 </body>
 <script>
-$(document).ready( function(){
+$(document).ready(function(){
 
-  $('#movieList p.item').on('click',function(){
+  $('body').on('click','.item',function(){
+    $('#searchText').val('');
     $('#viewport').fadeTo("fast", 0.4);
     $('#movieinfo').html('');
     $('#movietitle').html('Loading...');
-    $('#movieList p.item').css('background-color','black');
+    $('.item').css('background-color','');
     $(this).css('background-color','red');
 
     $.getJSON(window.location.href+'?moviename='+encodeURI($(this).text()),function(data){
@@ -151,8 +192,8 @@ $(document).ready( function(){
       if (result.backdrop_path !== null){
           $('#viewport').css('background-image', 'url('+'<?php echo $secure_base_url;?>'+result.backdrop_path+')').fadeTo("fast", 1);
           $('#noimage').remove();
-      } else {
-          $('#viewport').css('background-image', 'none').append('<p id="noimage">No movie image found.</p>');
+      } else{
+          $('#viewport').css('background-image', 'none').append('<p id="noimage">No movie image found.</p>').fadeTo("fast", 1);
       }
       attribution = '<p id="attribution">Movie info and graphics provided by <a href="https://themoviedb.org/" target="_blank">themoviedb.org</a></p><hr>';
       castlink = '<p id="castLink" data-movieid="'+result.id+'">Click here to show the cast.</p><div id="castBox"></div>';
@@ -174,7 +215,34 @@ $(document).ready( function(){
     });
   });
 
-  $('#movieList p.item').first().click();
+  $('#searchBox').keyup(function(e){
+    if (e.key==="Enter"){
+      $('#searchButton').click();
+    }
+  });
+
+  $('#searchButton').on('click',function(){
+    if(!$('#searchText').val())return;
+    //add a invisible item to the movielist, click it and delete it
+    var nList=document.createElement("p");
+    var t=document.createTextNode($('#searchText').val());
+    nList.appendChild(t);
+    nList.style.display="none";
+    nList.className="item";
+    document.getElementById("movieList").appendChild(nList);
+    $('.item').last().click();
+    document.getElementById("movieList").removeChild(nList);
+    //remove focus from searchText (to remove the android keyboard from screen)
+    //todo: set width and height to zero! (before adding to DOM)
+    var field = document.createElement('input');
+    field.setAttribute('type', 'text');
+    document.body.appendChild(field);
+    field.focus();
+    field.style.display="none";
+    document.body.removeChild(field);
+  });
+
+  $('.item').first().click();
 });
 </script>
 </html>
